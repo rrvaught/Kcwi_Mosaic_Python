Fixed Scaling and injected tests for tracking header changes

brutus.py
@@ -59,42 +59,82 @@ def calc_offsets(aimg, ahdr, img, hdr, box_length):
     # Initilaize grid search parameters #
     degperpix = abs(proj_plane_pixel_scales(awcs)[0])
     arcperpix = degperpix * 3600
-    print(arcperpix)
 
-    npix = round(box_length / arcperpix)
-    stepr = 0.5
-    step = 1.0
-    gx = 0
-    gy = 0
-    tryx = np.arange(-npix + gx, gx + npix + 1, step)
-    tryy = np.arange(-npix + gy, gy + npix + 1, step)
-    tryr = np.arange(-1, 1 + stepr, stepr)
+    attempt=0
 
-    match = 0.0
-    bestx, besty, bestr, test = 0, 0, 0, 0
+    while attempt < 2:
+        if attempt==0:
+            npix = round(box_length / arcperpix)
+            stepr = 0.5
+            step = 2.0
+            gx = -11.0
+            gy = 3.0
+            tryx = np.arange(-npix + gx, gx + npix + 1, step)
+            tryy = np.arange(-npix + gy, gy + npix + 1, step)
+            tryr = np.arange(-1, 1 + stepr, stepr)
 
-    i = 0
+            match = 0.0
+            bestx, besty, bestr, test = 0, 0, 0, 0
 
-    import itertools
-    for x in itertools.product(tryx, tryy, tryr):
-        im = rotate(shift(array, [x[1], x[0]], order=0, cval=-10), x[2], reshape=False)
-        test = abs(correl_mismatch(im, aimg))
+            i = 0
 
-        if np.isfinite(test) == False:
-            test = 0
-        while i == 0:
-            print('Initial Correlation = ', test)
-            match = test
-            i = 1
-        if test > match:
-            bestx = x[0]
-            besty = x[1]
-            bestr = x[2]
-            print('Updating best shift parameters to:')
-            print(bestx, besty, bestr, abs(test))
-            match = abs(test)
+            import itertools
+            for x in itertools.product(tryx, tryy, tryr):
+                im = rotate(shift(array, [x[1], x[0]], order=0, cval=-10), x[2], reshape=False)
+                test = abs(correl_mismatch(im, aimg))
+
+                if np.isfinite(test) == False:
+                    test = 0
+                while i == 0:
+                    print('Initial Correlation = ', test)
+                    match = test
+                    i = 1
+                if test > match:
+                    bestx = x[0]
+                    besty = x[1]
+                    bestr = x[2]
+                    print('First pass parameters updated to:')
+                    print(' bestx =',bestx,' besty =', besty, ' rot =',bestr, ' Corr =',abs(test))
+                    match = abs(test)
+                else:
+                    continue
+            attempt += 1
+
         else:
-            continue
+            npix = round(0.4/ arcperpix)
+            stepr = 0.5
+            step = 1.0
+            gx = bestx
+            gy = besty
+            tryx = np.arange(-npix + gx, gx + npix + 1, step)
+            tryy = np.arange(-npix + gy, gy + npix + 1, step)
+            tryr = np.arange(-1, 1 + stepr, stepr)
+
+            bestx, besty, bestr, test = 0, 0, 0, match
+
+            i = 0
+
+            import itertools
+            for x in itertools.product(tryx, tryy, tryr):
+                im = rotate(shift(array, [x[1], x[0]], order=0, cval=-10), x[2], reshape=False)
+                test = abs(correl_mismatch(im, aimg))
+
+                if np.isfinite(test) == False:
+                    test = 0
+                while i == 0:
+                    print('Initial Correlation = ', test)
+                    match = test
+                    i = 1
+                if test > match:
+                    bestx = x[0]
+                    besty = x[1]
+                    bestr = x[2]
+                    print('Updating best shift parameters to:')
+                    print(' bestx =', bestx, ' besty =', besty, ' rot =', bestr, ' Corr =', abs(test))
+                    match = abs(test)
+                else:
+                    continue
+            attempt += 1
 
     shift_x = degperpix * bestx
     shift_y = degperpix * besty

kcwi_align.py
@@ -1,8 +1,3 @@
-import numpy as np
-
-from kcwi_repro import repro
-
-
 def align_sf(list_of_images,directory):
     from kcwi_helper_functions import make_head_2d
     from kcwi_helper_functions import WcsUpdate
@@ -13,13 +8,19 @@ def align_sf(list_of_images,directory):
 
     anchor = list_of_images[0]
     anchdata = np.nansum(fits.open(anchor)[0].data[1000:,:,:], axis=0)
-    plt.imshow(anchdata)
-    plt.show()
-    anchhdr = make_head_2d(fits.open(anchor)[0].header)
+
+    header3d=fits.open(anchor)[0].header
+    anchhdr = make_head_2d(header3d)
     for image in list_of_images[1:]:
-        data = np.nansum(fits.open(image)[0].data[1000:,:,:], axis=0)
-        datahdr = make_head_2d(fits.open(image)[0].header)
-        sx, sy, sr= calc_offsets(anchdata, anchhdr, data, datahdr, 1.5)
+        imhdu=fits.open(image)
+        imdata=imhdu[0].data[1000:,:,:]
+        datahdr3d = imhdu[0].header
+        imhdu.close()
+
+        data = np.nansum(imdata, axis=0)
+        datahdr = make_head_2d(datahdr3d)
+
+        sx, sy, sr= calc_offsets(anchdata, anchhdr, data, datahdr, 2.5)
         WcsUpdate(image, sx, sy, sr, directory)
     return
 
@@ -42,10 +43,14 @@ def align_ri(list_of_images,ref_image,filter, directory):
     from reproject import reproject_interp
 
     # ensure ref image is north aligned
-    wcs_out, shape_out = find_optimal_celestial_wcs((fits.open(ref_image)[0].data,fits.open(ref_image)[0].header))
-    anchor, footprint = reproject_interp((fits.open(ref_image)[0].data,fits.open(ref_image)[0].header),
+    try:
+        wcs_out, shape_out = find_optimal_celestial_wcs((fits.open(ref_image)[0].data,fits.open(ref_image)[0].header))
+        anchor, footprint = reproject_interp((fits.open(ref_image)[0].data,fits.open(ref_image)[0].header),
                                            wcs_out, shape_out=shape_out)
-
+    except:
+        wcs_out, shape_out = find_optimal_celestial_wcs((fits.open(ref_image)[1].data, fits.open(ref_image)[1].header))
+        anchor, footprint = reproject_interp((fits.open(ref_image)[1].data, fits.open(ref_image)[1].header),
+                                             wcs_out, shape_out=shape_out)
     anchorhdr = wcs_out.to_header()
 
     filter_name = '/Users/rrickardsvaught/PycharmProjects/Kcwi_Mosaic_Python/Data/transmission_curves/'+filter
@@ -89,9 +94,8 @@ def align_ri(list_of_images,ref_image,filter, directory):
         n_anchhead['NAXIS'] = 2
         n_anchhead['NAXIS1'] = np.shape(cutout.data)[1]
         n_anchhead['NAXIS2'] = np.shape(cutout.data)[0]
-        print(n_anchhead)
 
-        sx, sy, sr = calc_offsets(cutout.data, n_anchhead, filt_image, datahdr,8.5)
+        sx, sy, sr = calc_offsets(cutout.data, n_anchhead, filt_image, datahdr,0.5)
         WcsUpdate(image, sx, sy, sr, directory)
 
     return

kcwi_trim.py
@@ -13,16 +13,22 @@ def ster2pix(fname,which):
         sqrarc_pix = header['PXSCL'] * header['SLSCL'] * 3600.0 ** 2
     except:
         sqrarc_pix = header['CDELT1'] * header['CDELT1'] * 3600.0 ** 2
-    ster_to_sqrarc=4.255e10
-    convfac=sqrarc_pix / ster_to_sqrarc
+
+    ster_to_sqrarc = 4.255e10
+    convfac= sqrarc_pix / ster_to_sqrarc
 
     if which=='sqrarc2ster':
         hdu[0].data=hdu[0].data / convfac
+        hdu.writeto(fname, overwrite=True)
+        hdu.close()
+        return convfac
     elif which=='ster2sqrarc':
         hdu[0].data = hdu[0].data * convfac
-    hdu.writeto(fname,overwrite=True)
-    hdu.close()
-    return convfac
+        hdu.writeto(fname, overwrite=True)
+        hdu.close()
+        return 1./convfac # returns inverse to scal back to flux per pix
+
+
 
 def make_wave(header, z=0):
     import numpy as np
@@ -58,8 +64,7 @@ def trim(fname,is_var=False):
     header=hdu[0].header
     new_header=header.copy()
     data = hdu[0].data
-    header.rename_keyword('CD3_3','CDELT3', force = False)
-    # Montage likes CDELT3 Keyword
+
     wave=make_wave(header)
 
     # Trim Wavelength
@@ -72,7 +77,9 @@ def trim(fname,is_var=False):
 
     new_header['NAXIS2'],new_header['NAXIS1']=np.shape(new_data)[1:]
     new_header['CRVAL3']=wave[gwave][0]
-    new_header['CDELT3'] = header['CDELT3']
+    # Montage likes CDELT3 Keyword
+    new_header['CDELT3'] = header['CD3_3']
+    new_header['CRPIX3'] = header['CRPIX3']
 
     tag='.trims.fits' if not is_var else '.vtrims.fits'
     out_fits=fits.HDUList([fits.PrimaryHDU(data=new_data,header=new_header)])